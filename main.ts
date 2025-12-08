350d9f5382fc3b88a9776f7c573cc78df6fb6e3b
Stefano Sambi <stefano.sambi@gmail.com>
2025-12-08 17:00:46 +0100

    second commit

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-let state = false
 input.onButtonPressed(Button.A, function () {
-    music.play(music.tonePlayable(523, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     state = !(state)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+})
+let state = false
+state = false
+basic.forever(function () {
     if (state) {
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.showIcon(IconNames.Yes)
